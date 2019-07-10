abc8d5eca37fdab39b1eaebc95dd8dda179035ff
SpekalsG3 <karpick.kirill@gmail.com>
2019-07-11 02:29:39 +0300

    Finished with static landing page. Added date range picker for come in and out.

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+$(".search-field[data-index='0']").dateRangePicker({
+	format: "DD.MM.YYYY",
+	separator: " to ",
+	getValue: function() {
+		if ($(".search-comein").val() && $(".search-comeout").val() )
+			return $(".search-comein").val() + " to " + $(".search-comeout").val();
+		else
+			return '';
+	},
+	setValue: function(s, s1, s2) {
+		$(".search-comein").val(s1);
+		$(".search-comeout").val(s2);
+	}
+});
+
+$(".search-field[data-index='1']").dateRangePicker({
+	autoClose: true,
+	singleDate : true,
+	showShortcuts: false,
+	singleMonth: true,
+	format: "DD.MM.YYYY",
+	setValue: function(s, s2) {
+		$(".search-comeout").val(s2);
+	}
+});
