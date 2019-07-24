4692cc70602d1cb1bb5f4eefd7fa15519d72f39d
SpekalsG3 <karpick.kirill@gmail.com>
2019-07-25 02:40:57 +0300

    Plugged doughnut chart plug-in

--- scripts/room.js
+++ scripts/room.js
@@ -1,23 +1,16 @@
 var data = [
-    { "CountryName": "China", "Pop1990": 1141 },
-    { "CountryName": "India", "Pop1990": 849 },
-    { "CountryName": "United States", "Pop1990": 250 },
-    { "CountryName": "Indonesia", "Pop1990": 178 },
-    { "CountryName": "Brazil", "Pop1990": 150 }
+    { "Оценка": "Хорошо", "Количество": 65 },
+    { "Оценка": "Великолепно", "Количество": 130 },
+    { "Оценка": "Удовлетворительно", "Количество": 65 }
 ];
 
-$(".room-total-graph").igDoughnutChart({
-    width: "100px",
-    height: "100px",
+$(".room-total-chart").igDoughnutChart({
+    innerExtent: 90,
     series:
     [{
-        name: "Pop1990",
-        labelMemberPath: "CountryName",
-        valueMemberPath: "Pop1990",
-        dataSource: data,
-        labelsPosition: "bestFit",
-        formatLabel: function (context) {
-            return context.itemLabel + " (" + context.item.Pop1990 + ")";
-        }
+        name: "Количество",
+        labelMemberPath: "Оценка",
+        valueMemberPath: "Количество",
+        dataSource: data
     }]
 });
