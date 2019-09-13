6ea0aa927e8d6c779814633d1dd18eed6aca8dd3
SpekalsG3 <karpick.kirill@gmai.com>
2019-09-14 01:01:07 +0300

    Broke one file with all components in several files

---
+++ src/pug/components/review/review.js
@@ -0,0 +1,11 @@
+$(".review__likes").click(function() {
+  var $this = $(this);
+  var flag = $this.attr("data-liked") === "true" ? "false" : "true";
+  $this.attr("data-liked", flag);
+  var $amount = $this.find(".review__amount");
+  if (flag === "true") {
+    $amount.text(parseInt($amount.text()) + 1);
+  } else {
+    $amount.text(parseInt($amount.text()) - 1);
+  }
+});
---
+++ src/pug/components/room-card/room-card.js
@@ -0,0 +1,61 @@
+var images = document.getElementsByClassName("room-card__images");
+var imagePoints = document.getElementsByClassName("room-card__points");
+var sliderIndexes = [];
+
+for (var i = 0; i < images.length; i++) {
+  var photos = images[i].children;
+  sliderIndexes[i] = 0;
+  for (var j = 0; j < photos.length; j++) {
+    photos[j].style.left = j * 100 + "%";
+  }
+}
+
+function SlidePrevPhoto(index) {
+  var thisImages = images[index].children;
+  for (var i = 0; i < thisImages.length; i++) {
+    thisImages[i].style.left = parseInt(thisImages[i].style.left) + 100 + "%";
+  }
+  imagePoints[index].children[sliderIndexes[index]].style.background = "none";
+  sliderIndexes[index]--;
+  imagePoints[index].children[sliderIndexes[index]].style.background = "#fff";
+}
+
+function SlideNextPhoto(index) {
+  var thisImages = images[index].children;
+  for (var i = 0; i < thisImages.length; i++) {
+    thisImages[i].style.left = parseInt(thisImages[i].style.left) - 100 + "%";
+  }
+  imagePoints[index].children[sliderIndexes[index]].style.background = "none";
+  sliderIndexes[index]++;
+  imagePoints[index].children[sliderIndexes[index]].style.background = "#fff";
+}
+
+$(".content__rooms a").click(function(e) {
+  e.preventDefault();
+  var targetClassName = e.target.classList[0];
+  if (targetClassName == "page__arrow" || targetClassName == "room-card__gallery--prev" || targetClassName == "room-card__gallery--next")
+    return;
+  window.location.href = e.currentTarget.href + "&" + window.location.search.slice(1);
+});
+
+$(".room-card__gallery--prev").click(function(e) {
+  var index = parseInt(this.parentNode.getAttribute("data-index"));
+  if (sliderIndexes[index] == 0) {
+    for (var i = 0; i < images[index].children.length-1; i++) {
+      SlideNextPhoto(index);
+    }
+  } else {
+    SlidePrevPhoto(index);
+  }
+});
+
+$(".room-card__gallery--next").click(function() {
+  var index = parseInt(this.parentNode.getAttribute("data-index"));
+  if (sliderIndexes[index] == images[index].children.length - 1) {
+    for (var i = 0; i < images[index].children.length-1; i++) {
+      SlidePrevPhoto(index);
+    }
+  } else {
+    SlideNextPhoto(index);
+  }
+});
