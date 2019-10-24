7f4cf877c8f19b13e47ad80c9bf381996a542a7e
SpekalsG3 <karpick.kirill@gmai.com>
2019-10-24 14:13:24 +0300

    Changed way images are loaded

--- src/components/page/page.js
+++ src/components/page/page.js
@@ -1,12 +1,18 @@
 import "./page.scss"
 
 import "../subscription/subscription.scss"
-import "../logo/logo.scss"
-import "../header/header.scss"
+import "../logo/logo.js"
+import "../header/header.js"
 import "../arrow/arrow.scss"
 import "../form/form.js"
 
 import "../../favicons/favicons.js"
+
+import twitterIcon from "./img/twitter-icon.svg";
+import facebookIcon from "./img/facebook-icon.svg";
+import instagramIcon from "./img/instagram-icon.svg";
+
+var footericons = [twitterIcon, facebookIcon, instagramIcon];
 
 var $content = $(".content");
 var $header = $(".header__content");
@@ -19,4 +25,8 @@ $(".header__menu").click(function() {
 window.addEventListener("touchmove", function () {
   $content.removeClass("content_shifted");
   $header.removeClass("header__content_shifted");
+});
+
+$(".footer__social-image").map((i, el) => {
+  el.setAttribute("src", footericons[i]);
 });
