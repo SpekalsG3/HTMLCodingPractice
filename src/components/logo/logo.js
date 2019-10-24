ce5f47dfc94aa11622a045cb9ba0a6531df617ed
SpekalsG3 <karpick.kirill@gmai.com>
2019-10-24 14:13:03 +0300

    Moved images that belong to specific blocks

---
+++ src/components/logo/logo.js
@@ -0,0 +1,7 @@
+import "./logo.scss";
+
+import logoIcon from "./img/logo-icon.svg";
+import logoBrand from "./img/logo-brand.svg";
+
+$(".logo__icon").attr("src", logoIcon);
+$(".logo__brand").attr("src", logoBrand);
